d71f51c40230ff06abb4c2c5bf7aaeef5c5ddad9
pranav <pranav@ccs.neu.edu>
2016-11-06 22:54:57 -0500

    assignment4 final
    assignment4 final

--- neu/webdev/public/assignment/views/user/user.controller.client.js
+++ neu/webdev/public/assignment/views/user/user.controller.client.js
@@ -1,12 +1,12 @@
 /**
  * Created by paran on 10/19/2016.
  */
-(function(){
+(function () {
     angular
         .module("WebAppMaker")
         .controller("LoginController", LoginController)
         .controller("ProfileController", ProfileController)
-        .controller("RegisterController",RegisterController);
+        .controller("RegisterController", RegisterController);
 
     function LoginController($location, UserService) {
         var vm = this;
@@ -18,46 +18,51 @@
             var promise = UserService.findUserByCredentials(username, password);
             console.log(promise);
             promise
-                .success(function(user){
-                    if(user === '0') {
+                .success(function (user) {
+                    if (user === '0') {
                         vm.error = "No such user";
                     } else {
                         console.log(user._id);
                         $location.url("/user/" + user._id);
                     }
                 })
-                .error(function(bbbb){
+                .error(function (bbbb) {
                     console.log(bbbb);
                 });
             /*if(user === null) {
-                vm.error = "No such user";
-            } else {
-                //console.log($location);
-                $location.url("/user/" + user._id);
-            }*/
+             vm.error = "No such user";
+             } else {
+             //console.log($location);
+             $location.url("/user/" + user._id);
+             }*/
         }
     }
 
     function RegisterController($location, UserService) {
-        var vm=this;
-        //vm.login = login
-        vm.register=register;
+        var vm = this;
+
+        vm.register = register;
 
         function register(user) {
+            if (vm.user.password !== vm.user.verifypassword) {
+                vm.error = "Passwords do not match!!";
+            }
+            else {
 
-            var promise = UserService.createUser(user);
-            promise
-                .success(function(user){
-                    if(user === '0') {
-                        vm.error = "No such user";
-                    } else {
-                        console.log(user._id);
-                        $location.url("/user/" + user._id);
-                    }
-                })
-                .error(function(bbbb){
-                    console.log(bbbb);
-                });
+                var promise = UserService.createUser(user);
+                promise
+                    .success(function (user) {
+                        if (user === '0') {
+                            vm.error = "No such user";
+                        } else {
+                            console.log(user._id);
+                            $location.url("/user/" + user._id);
+                        }
+                    })
+                    .error(function (bbbb) {
+                        console.log(bbbb);
+                    });
+            }
             // console.log("hello");
             // var user = UserService.createUser(user);
             // console.log(user);
@@ -68,12 +73,12 @@
             //     $location.url("/user/" + user._id);
             // }
 
-    }
+        }
     }
 
     function ProfileController($routeParams, UserService) {
         var vm = this;
-         //var usrid=parseInt($routeParams.uid);
+        //var usrid=parseInt($routeParams.uid);
         // vm.updateprofile=updateprofile;
         //
         // function updateprofile(user){
@@ -83,48 +88,50 @@
 
         var userId = parseInt($routeParams.uid);
         var user = UserService.findUserById(userId);
-        console.log("fdsfv"+userId);
+        console.log("fdsfv" + userId);
         console.log(user);
         vm.updateprofile = updateprofile;
         vm.unregisterUser = unregisterUser;
 
 
         function init() {
-           var promise = UserService.findUserById(userId);
+            var promise = UserService.findUserById(userId);
             promise
-                .success(function(user){
+                .success(function (user) {
                     //console.log(user);
-                    if(user != '0') {
+                    if (user != '0') {
                         vm.user = user;
                     }
                 })
-                .error(function(err){
-                        vm.error="err";
+                .error(function (err) {
+                    vm.error = "err";
                 });
 
 
         }
+
         init();
 
         function updateprofile() {
             console.log("inside update");
-            UserService.updateUser(userId,vm.user);
+            UserService.updateUser(userId, vm.user);
         }
 
         function unregisterUser() {
             UserService
                 .unregisterUser(vm.user._id)
-                .success(function(){
+                .success(function () {
                     $location.url("/login");
                 })
-                .error(function(){
+                .error(function () {
 
                 });
         }
+
         /*
-        if(user != null) {
-            vm.user = user;
-        }*/
+         if(user != null) {
+         vm.user = user;
+         }*/
     }
 
 })();
